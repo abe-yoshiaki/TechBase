d991c8dd11b0666770fbb454bf0fbd99ec1e19a2
siann55 <takodo_exword22@yahoo.co.jp>
2019-05-21 16:58:01 +0900

    記事投稿機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,21 @@
 class TweetsController < ApplicationController
+  # before_action :authenticate_user!
+
   def index
   end
 
   def home
+  end
+
+  def new
+  end
+
+  def create
+    Tweet.create(content: tweet_params[:text], user_id: current_user.id)
+  end
+
+  private
+  def tweet_params
+    params.permit(:text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'tweets/index'
   root 'tweets#home'
+  resources :tweets
   resources :users
 end
