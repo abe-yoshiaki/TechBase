f978fa3fb1ece947c4a2318bafb35bd4063a5dc3
siann55 <takodo_exword22@yahoo.co.jp>
2019-05-21 17:09:43 +0900

    記事一覧表示機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,7 @@ class TweetsController < ApplicationController
   # before_action :authenticate_user!
 
   def index
+    @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def home
