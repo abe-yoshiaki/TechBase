290ed7b48885c40268acab220ca4896338c19a60
siann55 <takodo_exword22@yahoo.co.jp>
2019-06-28 20:04:20 +0900

    syntaxerror解消

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,15 +11,11 @@ class TweetsController < ApplicationController
   end
 
   def create
-<<<<<<< Updated upstream
-    Tweet.create(content: tweet_params[:text], user_id: current_user.id)
-=======
     Tweet.create(title: tweet_params[:title], content: tweet_params[:text], user_id: current_user.id)
     if tweet_params[:title].present? && tweet_params[:text].present?
       redirect_to action: :index
       flash[:notice] = "JSのメッセージ表示用"
     end
->>>>>>> Stashed changes
   end
 
   private
