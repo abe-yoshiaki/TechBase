d423ecbf437f95764216e96ecc2cc946a52d67cf
siann55 <takodo_exword22@yahoo.co.jp>
2019-06-30 12:11:01 +0900

    マイページ画面

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    @MyTweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
+  end
+end
